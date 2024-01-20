35558fa871f2fa82e1485ea2579a355b7c2fb76b
Léo Carvalho <carvalho.csleo@gmail.com>
2024-01-19 23:38:31 -0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,10 +3,10 @@ set -e
 
 if [ ! -e '/var/www/html/index.php' ]; then
 	tar cf - --one-file-system -C /usr/src/tastyigniter . | tar xf -
-        chown -R www-data:www-data /var/www/html
         php artisan key:generate --force
 	php artisan igniter:install --no-interaction
 
 fi
+        chown -R www-data:www-data /var/www/html
 
 exec "$@"
